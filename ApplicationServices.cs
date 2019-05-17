99c6af7975b7c6a40bed17835fd57476d005b078
Adam Reilly <adammreilly@yahoo.com>
2019-05-17 07:21:23 -0500

    Split LispFunctions file into separate files to reflect the Autodesk API classes they work from.

---
+++ ApplicationServices.cs
@@ -0,0 +1,164 @@
+﻿using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.Runtime;
+using Autodesk.Civil.ApplicationServices;
+using System;
+
+
+namespace C3DtoLISP
+{
+    public partial class LispFunctions
+    {
+        // This region corresponds to the Autodesk.Civil.ApplicationServices.CivilDocument class
+        #region CivilDocument
+        // get the ObjectID collection of all surfaces in the drawing
+        [LispFunction("GetSurfaceIds")]
+        public ResultBuffer GetSurfaceIds(ResultBuffer resbuf)
+        {
+            var nil = new TypedValue((int)LispDataType.Nil);
+            if (resbuf == null)
+            {
+                CivilDocument currentDocument = CivilApplication.ActiveDocument;
+                ObjectIdCollection SurfaceIds = currentDocument.GetSurfaceIds();
+                ResultBuffer returnValue = ListObjectIds(SurfaceIds);
+                return returnValue;
+            }
+            return new ResultBuffer(nil);
+        }
+
+        // get the ObjectID collection of all alignments in the drawing
+        [LispFunction("GetAlignmentIds")]
+        public ResultBuffer GetAlignmentIds(ResultBuffer resbuf)
+        {
+            var nil = new TypedValue((int)LispDataType.Nil);
+            if (resbuf == null)
+            {
+                CivilDocument currentDocument = CivilApplication.ActiveDocument;
+                ObjectIdCollection alignmentIds = currentDocument.GetAlignmentIds();
+                ResultBuffer returnValue = ListObjectIds(alignmentIds);
+                return returnValue;
+            }
+            return new ResultBuffer(nil);
+        }
+
+        // get the ObjectID collection of all pipe networks in the drawing
+        [LispFunction("GetPipeNetworkIds")]
+        public ResultBuffer GetPipeNetworkIds(ResultBuffer resbuf)
+        {
+            var nil = new TypedValue((int)LispDataType.Nil);
+            if (resbuf == null)
+            {
+                CivilDocument currentDocument = CivilApplication.ActiveDocument;
+                ObjectIdCollection pipeNetworkIds = currentDocument.GetPipeNetworkIds();
+                ResultBuffer returnValue = ListObjectIds(pipeNetworkIds);
+                return returnValue;
+            }
+            return new ResultBuffer(nil);
+        }
+
+        // get the ObjectID collection of all the points in the drawing
+        [LispFunction("GetAllPointIds")]
+        public ResultBuffer GetAllPointIds(ResultBuffer resbuf)
+        {
+            var nil = new TypedValue((int)LispDataType.Nil);
+            if (resbuf == null)
+            {
+                CivilDocument currentDocument = CivilApplication.ActiveDocument;
+                ObjectIdCollection allPointIds = currentDocument.GetAllPointIds();
+                ResultBuffer returnValue = ListObjectIds(allPointIds);
+                return returnValue;
+            }
+            return new ResultBuffer(nil);
+        }
+
+        // get the ObjectID collection of all corridors in the drawing
+        [LispFunction("GetCorridorIds")]
+        public ResultBuffer GetCorridorIds(ResultBuffer resbuf)
+        {
+            var nil = new TypedValue((int)LispDataType.Nil);
+            if (resbuf == null)
+            {
+                CivilDocument currentDocument = CivilApplication.ActiveDocument;
+                Autodesk.Civil.DatabaseServices.CorridorCollection corridorColl = currentDocument.CorridorCollection;
+                ObjectIdCollection corridorIds = new ObjectIdCollection();
+                foreach (Autodesk.Civil.DatabaseServices.Corridor corr in corridorIds)
+                {
+                    corridorIds.Add(corr.ObjectId);
+                }
+                ResultBuffer returnValue = ListObjectIds(corridorIds);
+                return returnValue;
+            }
+            return new ResultBuffer(nil);
+        }
+
+        //
+        //TO DO: Add the following functions:
+        // GetAlignmentTableIds
+        // GetCivilDocument
+        // GetGeneralSegmentLabelIds
+        // GetIntersectionIds
+        // GetLegendTableIds
+        // GetNoteLabelIds
+        // GetParcelSegmentTableIds
+        // GetParcelTableIds
+        // GetPointTableIds
+        // GetSiteIds
+        // GetSitelessAlignmentId
+        // GetSitelessAlignmentIds
+        // GetViewGrameGroupIds
+        // AssemblyCollection
+        // CogoPoints
+        // CorridorState
+        // IsCorridorSectionViewActive
+        // IsDriveActive
+        // IsSectoinEditorCorridorReferenceObject
+        // NetworkState
+        // PointGroups
+        // PointUDPClassifications
+        // PointUDPs
+        // Settings
+        // Styles
+        // SubassemblyCollection
+        //
+
+        #endregion
+
+        // This region corresponds to the Autodesk.Civil.ApplicationServices.CivilApplication class
+        #region CivilApplication
+
+        //
+        // TO DO: Add:
+        // ActiveDocument
+        // ActiveProduct
+        // SurveyProjects
+        //
+
+
+        #endregion
+
+        #region ProductType
+            // should return the string value of the number provided as a product type
+        [LispFunction("ProductType")]
+        public ResultBuffer ProductType(ResultBuffer resbuf)
+        {
+            var nil = new TypedValue((int)LispDataType.Nil);
+            if (resbuf == null)
+            {
+                var args = resbuf.AsArray();
+                if (args.Length == 1)
+                {
+                   ResultBuffer returnValue = new ResultBuffer
+                        {
+                        Autodesk.Civil.ApplicationServices.ProductType.GetName(typeof(Autodesk.Civil.ApplicationServices.ProductType), 0)
+                        };
+                   return returnValue;
+                }
+            }
+            return new ResultBuffer(nil);
+        }
+
+
+        #endregion
+
+    }
+}
---
+++ UtilityFunctions.cs
@@ -0,0 +1,68 @@
+﻿using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Runtime;
+using Autodesk.Civil.ApplicationServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace C3DtoLISP
+{
+    public partial class LispFunctions
+    {
+        // this file is intended to house utility functions that can be reused by the other functions
+
+        // check if the result buffer is not null, and a single input value of an ObjectId
+        private bool BufferContainsObjectId(ResultBuffer resbuf)
+        {
+            // if the resbuf is empty, return nil
+            if (resbuf == null)
+                return false;
+            // check if the resbuf is more than one argument
+            var args = resbuf.AsArray();
+            if (args.Length != 1)
+                return false;
+            // verify the resbuf is an objectId
+            if (args[0].TypeCode != (int)LispDataType.ObjectId)
+                return false;
+            return true;
+        }
+
+        //creates a LISP compatable list of ObjectIds from an ObjectIdCollection
+        public ResultBuffer ListObjectIds(ObjectIdCollection objectIds)
+        {
+            var nil = new TypedValue((int)LispDataType.Nil);
+            try
+            {
+                using (Transaction ts = Application.DocumentManager.MdiActiveDocument.Database.TransactionManager.StartTransaction())
+                {
+                    // iterate thru the object ids and create a list to return to LISP
+                    ResultBuffer returnValue = new ResultBuffer();
+                    if (objectIds.Count == 0)
+                        return new ResultBuffer(nil);
+                    returnValue.Add(new TypedValue((int)LispDataType.ListBegin));
+                    foreach (ObjectId objectId in objectIds)
+                    {
+                        var item = (new TypedValue((int)LispDataType.ObjectId, objectId));
+                        returnValue.Add(item);
+                    }
+                    returnValue.Add(new TypedValue((int)LispDataType.ListEnd));
+                    return returnValue;
+                }
+            }
+            catch (Autodesk.AutoCAD.Runtime.Exception ex)
+            {
+                // any error will dump a message to the console and return nil
+                Console.WriteLine(ex.Message);
+                return new ResultBuffer(nil);
+            }
+            // if something else happens, return nil
+            return new ResultBuffer(nil);
+        }
+        
+    }
+}
